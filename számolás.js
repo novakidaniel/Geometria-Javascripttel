027362120f6379551b75c455f8e3e53546b984a2
novakidaniel <novaki.daniel@students.jedlik.eu>
2021-05-05 13:28:54 +0200

    téglalap számolás

--- számolás.js
+++ számolás.js
@@ -7,3 +7,13 @@ terület = a*a;
 document.getElementById("nkerület").innerHTML="A négyzet kerülete: "+kerület;
 document.getElementById("nterület").innerHTML="A négyzet területe: "+terület;
 }
+
+function téglalap( )
+{
+var magasság = document.getElementById('magasság').value;
+var szélesség = document.getElementById('szélesség').value;
+var tkerület = 2*(+magasság + +szélesség);
+var tterület = magasság * szélesség;
+document.getElementById("tkerület").innerHTML="A téglalap kerülete: "+tkerület;
+document.getElementById("tterület").innerHTML="A téglalap területe: "+tterület;
+}
